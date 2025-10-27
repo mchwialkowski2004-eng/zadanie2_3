cdf8db8a36edd59781bd1ae72695845bbdf673f4
mchwialkowski2004-eng <mchwialkowski2004@gmail.com>
2025-10-27 10:22:45 +0000

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("podaj liczbe a");
+    int a = scanner.nextInt();
+    System.out.println("podaj liczbe b");
+    int b = scanner.nextInt();
+    int wynik = a * a + b * b;
+    System.out.println("wynik to " + wynik);
+    scanner.close();
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
 }
